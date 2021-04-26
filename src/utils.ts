d14a6710c9ffba1ddfb5c3a920d8394c9ea2828c
Dan Bennett <dghbennett47@gmail.com>
2021-04-26 01:16:54 +0100

    feat: add shuffle function

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,10 @@
-export {};
+export const shuffleArray = (array: any[]) => {
+	const copyArray: any[] = array.slice(0);
+	for (let i: number = copyArray.length - 1; i > 0; i--) {
+		let j: number = Math.floor(Math.random() * (i + 1));
+		let temp: any = copyArray[i];
+		copyArray[i] = copyArray[j];
+		copyArray[j] = temp;
+	}
+	return copyArray;
+};
